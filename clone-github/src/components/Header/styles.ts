5a86c49d24c46ea1514dddf58477ddba8083b37e
eduardozuppodev <zuppo.edu@gmail.com>
2021-05-24 11:04:45 -0300

    .

--- clone-github/src/components/Header/styles.ts
+++ clone-github/src/components/Header/styles.ts
@@ -1,5 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background: var(--header);
+  padding: 11px 16px;
+`;
 
+export const GithubLogo = styled(FaGithub)`
+  fill: var(--logo);
+  width: 32px;
+  height: 32px;
+  flex-shrink: 0;
+`;
+
+export const SearchForm = styled.form`
+    padding-left: 16px;
 `;

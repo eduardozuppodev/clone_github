506fbacac0255411b86faa16115d30b8468e5236
eduardozuppodev <zuppo.edu@gmail.com>
2021-05-25 08:58:53 -0300

    .

--- clone-github/src/components/RepoCard/index.tsx
+++ clone-github/src/components/RepoCard/index.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from './styles';
 
-// import { Container } from './styles';
-
-const RepoCard: React.FC = () => {
-  return <div />;
+interface Props {
+  username: string;
+  reponame: string;
+  description?: string;
+  language?: string;
+  stars: number;
+  forks: number;
 }
+
+const RepoCard: React.FC<Props> = ({
+  username,
+  reponame,
+  description,
+  language,
+  stars,
+  forks,
+}) => {
+  const languageClass = language ? language.toLowerCase() : 'other';
+
+  return (
+    <Container>
+      <Topsite>
+        <header>
+          <RepoIcon />
+          <Link to={`/${username}/${reponame}`}>{reponame}</Link>
+        </header>
+
+        <p>{description}</p>
+      </Topsite>
+
+      <Botside>
+        <ul>
+          <li>
+            <div className={`language ${languageClass}`}></div>
+            <span>{language}</span>
+          </li>
+          <li>
+            <StarIcon />
+          </li>
+        </ul>
+      </Botside>
+    </Container>
+  );
+};
 
 export default RepoCard;

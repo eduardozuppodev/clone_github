d71bf5196486f6cb7090c391b03f281244f1f8d6
eduardozuppodev <zuppo.edu@gmail.com>
2021-05-26 09:03:10 -0300

    .

--- clone-github/src/pages/Repo/index.tsx
+++ clone-github/src/pages/Repo/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/eduardozuppodev'}>
+          eduardozuppodev
+      </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/eduardozuppodev/github-clone'}>
+          github-clone
+        </Link>
+
+      </Breadcrumb>
+
+      <p>Clone of the Github profile page</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>starts</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/eduardozuppodev/github_clone'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
